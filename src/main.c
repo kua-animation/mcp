6671950ad6167600175071ab3ddfd6f720542c99
kua-animation <andrej.ciperle@gmail.com>
2025-03-01 22:22:42 +0100

    refector to 3d

--- src/main.c
+++ src/main.c
@@ -1,91 +1,61 @@
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "include/graphic.h"
-#include "include/physics.h"
 
-#define MCP_PARTICLE 15
-#define MCP_WINDOW_W 1200
-#define MCP_WINDOW_H 400 
+void init(int w, int h) {
+	glEnable(GL_DEPTH_TEST);
 
-SDL_Window* CreateWindow(int width, int height, const char* title);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f, (float)w/ (float)h, 0.1f, 100.0f);
+	glMatrixMode(GL_MODELVIEW);
+}
 
-SDL_Renderer* CreateRenderer(SDL_Window* window);
+void render(SDL_Window* window) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-MCPPosition Pos[MCP_PARTICLE];
+	glLoadIdentity();
+	
+	glBegin(GL_QUADS);
+	glColor3f(1.0f, 0.6f, 0.3f);
+	glVertex3f(-0.5f, -0.5f, 0.0f);
+	glVertex3f(0.5f, -0.5f, 0.0f);
+	glVertex3f(0.5f, 0.5f, -3.0f);
+	glVertex3f(-0.5f, 0.5f, -3.0f);
+	glEnd();
 
-int main(int argc, char *argv[]) {
-	SDL_Window* Win;
-	Win = CreateWindow(MCP_WINDOW_W, MCP_WINDOW_H, "andrej");
-	SDL_Renderer* Ren = CreateRenderer(Win);
+	SDL_GL_SwapWindow(window);
 
-	SDL_Event event;
+}
+
+int main(int argc, char* argv[]) {
+	SDL_Window* window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	glewInit();
+	
+	init(800, 600);
+
 	int running = 1;
-	int MovIng = 0;
+	SDL_Event event;
 
-	int DirX = 0;
-	int DirY = 0;
-
-	Pos[MCP_PARTICLE-1].X = 200/MCP_PARTICLE;
-	Pos[MCP_PARTICLE-1].Y = 200/MCP_PARTICLE;
-
-	Pos[0].X = 2;
-	Pos[0].Y = 0;
-
-	MCPColor colorline = {180, 255, 210, 255};
-	MCPColor colorPoints = {210, 100, 120, 255};
-
-	while (running)	{
+	while (running) {
 		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT ) {
-				running = 0;				
-			}
-			if (WasButtonPress(event)) {
-				if (MovIng) {
-					MovIng = 0;
-				} else {
-					MovIng = 1;
-				}
+			if(event.type == SDL_QUIT) {
+				running = 0;
 			}
 		}
-		
-		if (MCP_Physics_Move(Pos[0], MCP_WINDOW_W, MCP_WINDOW_H)) {
-			printf("a");
-			fflush(stdout);
-		}
 
-		SDL_GetMouseState(&Pos[0].X, &Pos[0].Y);
-
-		SDL_SetRenderDrawColor(Ren, 0, 0, 0, 255);
-		SDL_RenderClear(Ren);
-		if (MovIng) {
-			MCP_Physics_Points(Ren, Pos, MCP_PARTICLE, colorPoints);
-		} else { 
-			MCP_Physics_Chain(Ren, Pos, MCP_PARTICLE, 0, colorPoints, colorline);
-		}
-		SDL_RenderPresent(Ren);
-
+		render(window);
 		SDL_Delay(10);
 	}
 
-	SDL_DestroyWindow(Win);
-	SDL_DestroyRenderer(Ren);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
-
-	return 0; }
-
-
-SDL_Window* CreateWindow(int width, int height, const char* title) {
-	SDL_Window* window = SDL_CreateWindow(title, 
-				SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-				width, height, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-
-	return window;
+	return 0;
 }
-
-SDL_Renderer* CreateRenderer(SDL_Window* window) {
-	SDL_Renderer* render = SDL_CreateRenderer(window, -1, 0);
-	
-	return render;
-}
-
 
